4f78fb86d9d13a8a82d1ca0c61b26708f85d5e52
DJ <davidsalinasportilla@gmail.com>
2023-08-10 22:23:22 -0500

    nuevo intsrumento

--- src/main/js/pages/nuevo-instrumento.js
+++ src/main/js/pages/nuevo-instrumento.js
@@ -4,16 +4,18 @@ const { Link } = require('react-router-dom');
 const client = require('../client');
 
 
-const NuevoMusicoPage = () => {
+const NuevoInstrumentoPage = () => {
 
     const [nombre, setNombre] = useState('')
+    const [categoria, setCategoria] = useState('')
+    const [descripcion, setDescripcion] = useState('')
 
     const handleSubmit = (evento)=>{
         evento.preventDefault();
         client({
             method: 'POST',
-            path: '/api/musicos',
-            entity: {nombre: nombre},
+            path: '/api/instrumentos',
+            entity: {nombre: nombre, categoria:categoria, descripcion:descripcion},
             headers: {'Content-Type': 'application/json'}
         }).done(()=>{
             window.location = '/';
@@ -22,15 +24,19 @@ const NuevoMusicoPage = () => {
 
     return (
         <>
-        <h1>Nuevo Músico</h1>
+        <h1>Nuevo Instrumento</h1>
         <form onSubmit={handleSubmit}>
             <label>Nombre</label> <br />
             <input type="text" id='nombre' name='nombre' onChange={e=>setNombre(e.target.value)} /> <br />
-            <input type="submit" value="Nuevo Músico" />
+            <label>Categoria</label> <br />
+            <input type="text" id='categoria' name='categoria' onChange={e=>setCategoria(e.target.value)} /> <br />
+            <label>Descripción</label> <br />
+            <input type="text" id='descripcion' name='descripcion' onChange={e=>setDescripcion(e.target.value)} /> <br />
+            <input type="submit" value="Nuevo Instrumenmto" />
         </form>
         <Link to="/">Volver</Link>
         </>
     )
 }
 
-module.exports = NuevoMusicoPage;
+module.exports = NuevoInstrumentoPage;
